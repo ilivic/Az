abce3778d6aa4f7ecebaa1efd841cb487012954d
unknown <ilivic@yandex.ru>
2024-11-18 00:38:02 +0300

    commit 18.11.2024

--- Hr_manager/Hr_manager/VisualApp/PagesApp/PageCreateX.xaml.cs
+++ Hr_manager/Hr_manager/VisualApp/PagesApp/PageCreateX.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hr_manager.ADOApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Hr_manager.VisualApp.PagesApp
 {
     /// <summary>
@@ -20,10 +22,49 @@ namespace Hr_manager.VisualApp.PagesApp
     /// </summary>
     public partial class PageCreateX : Page
     {
+        public static List<InternTask> _IT {  get; set; }
         public PageCreateX()
         {
             InitializeComponent();
             CMBIntern.ItemsSource = App.Connection.Intern.Where(z=>z.IsActive==true).ToList();
+        }
+
+        private void CreateXReport(object sender, RoutedEventArgs e)
+        {
+            var _selIntern = CMBIntern.SelectedItem as Intern;
+            if (_selIntern != null) 
+            {
+                _IT = new List<InternTask> (App.Connection.InternTask.Where(z => z.Intern_id == _selIntern.Id_intern).ToList());
+                DGT.ItemsSource = _IT;
+                var _complit = _IT.Where(z => z.Task.Status_id == 3).ToList().Count;
+                var _Nocomplit = _IT.Where(z => z.Task.Status_id == 2 || z.Task.Status_id == 4).ToList().Count;
+                var _All = _IT.ToList().Count;
+                TxtCount.Text = $"Стажер {_selIntern.FullName} \nна данный момент имеет {_All} \nиз которых выполненно {_complit} \nне выполненно {_Nocomplit}";
+                var Answer = "";
+                if (_complit > _Nocomplit)
+                {
+                    Answer = "отлично";
+                }
+                else if (_complit == _Nocomplit)
+                { 
+                    Answer = "удовлетворительно";
+                }
+                else
+                {
+                    Answer = "плохо";
+                }
+                var result = 100/(_All / _complit);
+                TxtStatistick.Text = $"На данный момент деятельность стажера оценивается как {Answer} поскольку он выполняет {result}% заявок ";
+            }
+        }
+
+        private void ClEventCreateSelImport(object sender, RoutedEventArgs e)
+        {
+            var _selIntern = CMBIntern.SelectedItem as Intern;
+            if (_selIntern != null)
+            {
+
+            }
         }
     }
 }
