ca4bfb733af8c7ccbcec41a1a419aea5b3454880
unknown <ilivic@yandex.ru>
2024-11-19 01:33:06 +0300

    commit 19.11.2024

--- Hr_manager/Hr_manager/VisualApp/PagesApp/PageCreateX.xaml.cs
+++ Hr_manager/Hr_manager/VisualApp/PagesApp/PageCreateX.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Hr_manager.ADOApp;
+using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using _excel = Microsoft.Office.Interop.Excel;
+using Page = System.Windows.Controls.Page;
 
 
 namespace Hr_manager.VisualApp.PagesApp
@@ -23,6 +27,10 @@ namespace Hr_manager.VisualApp.PagesApp
     public partial class PageCreateX : Page
     {
         public static List<InternTask> _IT {  get; set; }
+        public static int _complit {  get; set; }
+        public static int _Nocomplit {  get; set; }
+        public static int _All {  get; set; }
+        public static double result {  get; set; }
         public PageCreateX()
         {
             InitializeComponent();
@@ -36,9 +44,9 @@ namespace Hr_manager.VisualApp.PagesApp
             {
                 _IT = new List<InternTask> (App.Connection.InternTask.Where(z => z.Intern_id == _selIntern.Id_intern).ToList());
                 DGT.ItemsSource = _IT;
-                var _complit = _IT.Where(z => z.Task.Status_id == 3).ToList().Count;
-                var _Nocomplit = _IT.Where(z => z.Task.Status_id == 2 || z.Task.Status_id == 4).ToList().Count;
-                var _All = _IT.ToList().Count;
+                _complit = _IT.Where(z => z.Task.Status_id == 3).ToList().Count;
+                _Nocomplit = _IT.Where(z => z.Task.Status_id == 2 || z.Task.Status_id == 4).ToList().Count;
+                _All = _IT.ToList().Count;
                 TxtCount.Text = $"Стажер {_selIntern.FullName} \nна данный момент имеет {_All} \nиз которых выполненно {_complit} \nне выполненно {_Nocomplit}";
                 var Answer = "";
                 if (_complit > _Nocomplit)
@@ -53,7 +61,7 @@ namespace Hr_manager.VisualApp.PagesApp
                 {
                     Answer = "плохо";
                 }
-                var result = 100/(_All / _complit);
+                result = 100/(_All / _complit);
                 TxtStatistick.Text = $"На данный момент деятельность стажера оценивается как {Answer} поскольку он выполняет {result}% заявок ";
             }
         }
@@ -63,8 +71,43 @@ namespace Hr_manager.VisualApp.PagesApp
             var _selIntern = CMBIntern.SelectedItem as Intern;
             if (_selIntern != null)
             {
-
+                _Application excel = new _excel.Application();
+                Workbook _wb;
+                Worksheet _ws;
+                _wb = excel.Workbooks.Add();
+                _ws = _wb.Worksheets[1];
+                _ws.Cells[1, 1].Value2 = "Стажер";
+                _ws.Cells[1, 2].Value2 = _selIntern.FullName;
+                _ws.Cells[1, 1].Value2 = "Работает с ";
+                _ws.Cells[2, 2].Value2 = _selIntern.DataStart;
+                _ws.Cells[3, 1].Value2 = "процент выполнения заявок";
+                _ws.Cells[3, 2].Value2 = result;
+                _ws.Cells[4, 1].Value2 = "Название";
+                _ws.Cells[4, 2].Value2 = "Место выполнения";
+                _ws.Cells[4, 3].Value2 = "Статус";
+                _ws.Cells[4, 4].Value2 = "Дата начала выполнения";
+                _ws.Cells[4, 5].Value2 = "Дата сдачи";
+                _ws.Cells[4, 6].Value2 = "Дата выдачи задания"; 
+                var i = 5;
+                foreach (var index in _IT.ToList())
+                {
+                    _ws.Cells[i, 1].Value2 = index.Task.Title;
+                    _ws.Cells[i, 2].Value2 = index.Task.Location.Locc;
+                    _ws.Cells[i, 3].Value2 = index.Task.Status.Info;
+                    _ws.Cells[i, 4].Value2 = index.DateBegin;
+                    _ws.Cells[i, 5].Value2 = index.DateEnd;
+                    _ws.Cells[i, 6].Value2 = index.Data;
+                    i++;
+                }
+                _wb.SaveAs("ReportX.xlsx");
+                _wb.Close();
             }
+        }
+
+
+        private void ClEventRevers(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PageMainMenu());
         }
     }
 }
